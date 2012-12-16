c7b5aa79ef2560492ae98e2bbdcb77ff748a392f
Travis Ralston <travpc@gmail.com>
2012-12-15 20:38:19 -0700

    Remove old files

--- inc/constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("URL", "http://xmail.turt2live.com/2/");
-define("MOBILE_URL", "http://xmail.turt2live.com/2/mobile");
-define("TABLET_URL", "http://xmail.turt2live.com/2/tablet");
-?>
