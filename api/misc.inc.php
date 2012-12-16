f3090a1a3cdea332ff8db18b036676ff677ead8c
Travis Ralston <travpc@gmail.com>
2012-12-15 20:38:37 -0700

    Changed API function

--- api/misc.inc.php
+++ api/misc.inc.php
@@ -3,11 +3,13 @@ function onRegister($user){
 	
 }
 
+// Commented until perfected
+
 function onSend($to, $from, $message){
 	$query = mysql_query("SELECT `email` FROM `users` WHERE `username`='$to'");
 	if(mysql_num_rows($query)>0){
 		$email = mysql_result($query, 0, "email");
-		mail($email, "New mail from $from!", "You have new mail from $from, you can reply to this email to send a message back! Here's the message:\n\n".dirty($message), "From: $from@xmail.turt2live.com\r\nReply-To: $from@xmail.turt2live.com\r\nReturn-Path: $from@xmail.turt2live.com");
+		//mail($email, "New mail from $from!", "You have new mail from $from, you can reply to this email to send a message back! Here's the message:\n\n".dirty($message), "From: $from@xmail.turt2live.com\r\nReply-To: $from@xmail.turt2live.com\r\nReturn-Path: $from@xmail.turt2live.com");
 	}
 }
 
@@ -15,7 +17,7 @@ function onComplexSend($to, $from, $message){
 	$query = mysql_query("SELECT `email` FROM `users` WHERE `username`='$to'");
 	if(mysql_num_rows($query)>0){
 		$email = mysql_result($query, 0, "email");
-		mail($email, "New mail from $from! (in-game attachments)", "You have new mail (with in-game attachments) from $from, you can reply to this email to send a message back, but don't forget to login to an xMail server to get your attachments! Here's the message:\n\n".dirty($message), "From: $from@xmail.turt2live.com\r\nReply-To: $from@xmail.turt2live.com\r\nReturn-Path: $from@xmail.turt2live.com");
+		//mail($email, "New mail from $from! (in-game attachments)", "You have new mail (with in-game attachments) from $from, you can reply to this email to send a message back, but don't forget to login to an xMail server to get your attachments! Here's the message:\n\n".dirty($message), "From: $from@xmail.turt2live.com\r\nReply-To: $from@xmail.turt2live.com\r\nReturn-Path: $from@xmail.turt2live.com");
 	}
 }
 ?>
