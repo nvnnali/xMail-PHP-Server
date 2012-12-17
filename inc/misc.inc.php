cacd278fb41527e10efe1978d67272c3169b4928
Travis Ralston <travpc@gmail.com>
2012-12-16 23:01:07 -0700

    Import from /mail

---
+++ inc/misc.inc.php
@@ -0,0 +1,27 @@
+<?php
+/*
+MISC FUNCTIONS USED BY SERVER
+*/
+function valid($input){
+	$input = trim($input);
+	return isset($input) && $input!=null && $input!="";
+}
+
+function clean($input){
+	if(get_magic_quotes_gpc()){
+		$input = stripslashes($input);
+	}
+	$input = mysql_real_escape_string($input);
+	$input = htmlentities($input, ENT_COMPAT, "UTF-8");
+	return $input;
+}
+
+function dirty($input){
+	//if(get_magic_quotes_gpc()){
+	//	$input = addslashes($input);
+	//}
+	//$input = mysql_real_escape_string($input);
+	$input = html_entity_decode($input, ENT_COMPAT, "UTF-8");
+	return $input;
+}
+?>
