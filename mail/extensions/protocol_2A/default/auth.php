6d90fbdf95005e2d53cb12cfb15b2f2158e42cf2
Travis Ralston <travpc@gmail.com>
2013-08-26 14:14:08 -0600

    Use force variable in generateNewAuth

--- mail/extensions/protocol_2A/default/auth.php
+++ mail/extensions/protocol_2A/default/auth.php
@@ -2,19 +2,20 @@
 function generateNewAuth($force=false){
     $ip = $_SERVER['REMOTE_ADDR'];
     global $sqlConn;
-    $query = mysqli_query($sqlConn, "SELECT `lastcheck` FROM `xmail_server_connections` WHERE `ip`='{$ip}' LIMIT 1") or die(mysqli_error($sqlConn));
-    if(mysqli_num_rows($query)>0){
-        $lastCheck = mysqli_result($query, 0, "lastcheck");
-        // TODO: Proper abuse check
-        // Use "FORCE" var!!
-        /*if(time()-$lastCheck<3600){
-            header("HTTP/1.0 403 Forbidden");
-            echo json_encode(array("message" => "Access Denied"));
-            die();
-        }else{*/
-            mysqli_query($sqlConn, "DELETE FROM `xmail_server_connections` WHERE `ip`='{$ip}' LIMIT 1") or die(mysqli_error($sqlConn));
-        //}
-    }    
+    if(!$force){
+        $query = mysqli_query($sqlConn, "SELECT `lastcheck` FROM `xmail_server_connections` WHERE `ip`='{$ip}' LIMIT 1") or die(mysqli_error($sqlConn));
+        if(mysqli_num_rows($query)>0){
+            $lastCheck = mysqli_result($query, 0, "lastcheck");
+            // TODO: Proper abuse check
+            /*if(time()-$lastCheck<3600){
+                header("HTTP/1.0 403 Forbidden");
+                echo json_encode(array("message" => "Access Denied"));
+                die();
+            }else{*/
+                mysqli_query($sqlConn, "DELETE FROM `xmail_server_connections` WHERE `ip`='{$ip}' LIMIT 1") or die(mysqli_error($sqlConn));
+            //}
+        }    
+    }
     
     $now = time();
     
