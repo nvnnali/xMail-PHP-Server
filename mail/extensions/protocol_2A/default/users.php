4af0e70444bc13119e65a60198883ef5ba7ed85d
Travis Ralston <travpc@gmail.com>
2013-08-28 08:38:33 -0600

    Use a secondary table for these functions

--- mail/extensions/protocol_2A/default/users.php
+++ mail/extensions/protocol_2A/default/users.php
@@ -2,9 +2,12 @@
 define("USERS_TABLE_NAME", "xmail_users");
 define("USERS_USERNAME_COL", "username");
 define("USERS_PASSWORD_COL", "password"); // Note: be sure to change the password policy if needed in the config.php
+
+define("USERS_LOGIN_TABLE_NAME", "xmail_users_login");
+define("USERS_IP_COL", "ip");
+define("USERS_LOGIN_USERNAME_COL", "username");
 define("USERS_LOGGEDIN_COL", "loggedIn");
 
-// Only set the following if USERS_USE_MAIN_SQL is false
 define("USERS_MYSQL_HOST", "localhost");
 define("USERS_MYSQL_USER", "xmail");
 define("USERS_MYSQL_PASS", "xmail");
@@ -13,7 +16,7 @@ define("USERS_MYSQL_DATABASE", "xmail");
 // From here on in there should be no need for editing
 
 // Load sql
-$sqlCon = mysqli_connect(constant("USERS_MYSQL_HOST"), constant("USERS_MYSQL_USER"), constant("USERS_MYSQL_PASS")); // need true for "new connection"
+$sqlCon = mysqli_connect(constant("USERS_MYSQL_HOST"), constant("USERS_MYSQL_USER"), constant("USERS_MYSQL_PASS"));
 $sqlDb = mysqli_select_db($sqlCon, constant("USERS_MYSQL_DATABASE"));
 
 function isUser($name){
@@ -26,7 +29,8 @@ function loginUser($name, $hashedPassword){
     global $sqlCon;
     $query = mysqli_query($sqlCon, "SELECT * FROM `".constant("USERS_TABLE_NAME")."` WHERE `".constant("USERS_USERNAME_COL")."`='".$name."' AND `".constant("USERS_PASSWORD_COL")."`='".$hashedPassword."' LIMIT 1");
     if(mysqli_num_rows($query)>0){
-        mysqli_query($sqlCon, "UPDATE `".constant("USERS_TABLE_NAME")."` SET `".constant("USERS_LOGGEDIN_COL")."`='1' WHERE `".constant("USERS_USERNAME_COL")."`='".$name."' AND `".constant("USERS_PASSWORD_COL")."`='".$hashedPassword."' LIMIT 1") or die(mysqli_error($sqlCon));
+        $ip = $_SERVER['REMOTE_ADDR'];
+        mysqli_query($sqlCon, "UPDATE `".constant("USERS_LOGIN_TABLE_NAME")."` SET `".constant("USERS_LOGGEDIN_COL")."`='1' WHERE `".constant("USERS_LOGIN_USERNAME_COL")."`='".$name."' AND `".constant("USERS_IP_COL")."`='".$ip."' LIMIT 1") or die(mysqli_error($sqlCon));
         return true;
     }
     return false;
@@ -34,18 +38,19 @@ function loginUser($name, $hashedPassword){
 
 function registerUser($name, $hashedPassword){
     global $sqlCon;
-    mysqli_query($sqlCon, "INSERT INTO `".constant("USERS_TABLE_NAME")."` (`".constant("USERS_USERNAME_COL")."`, `".constant("USERS_PASSWORD_COL")."`, `".constant("USERS_LOGGEDIN_COL")."`) VALUES ('".$name."', '".$hashedPassword."', '1')") or die(mysqli_error($sqlCon));
+    $ip = $_SERVER['REMOTE_ADDR'];
+    mysqli_query($sqlCon, "UPDATE `".constant("USERS_LOGIN_TABLE_NAME")."` SET `".constant("USERS_LOGGEDIN_COL")."`='0' WHERE `".constant("USERS_LOGIN_USERNAME_COL")."`='".$name."' AND `".constant("USERS_IP_COL")."`='".$ip."' LIMIT 1") or die(mysqli_error($sqlCon));
     return true;
 }
 
 function logoutUser($name){
     global $sqlCon;
-    mysqli_query($sqlCon, "UPDATE `".constant("USERS_TABLE_NAME")."` SET `".constant("USERS_LOGGEDIN_COL")."`='0' WHERE `".constant("USERS_USERNAME_COL")."`='".$name."' LIMIT 1") or die(mysqli_error($sqlCon));
+    mysqli_query($sqlCon, "UPDATE `".constant("USERS_LOGIN_TABLE_NAME")."` SET `".constant("USERS_LOGGEDIN_COL")."`='0' WHERE `".constant("USERS_LOGIN_USERNAME_COL")."`='".$name."' LIMIT 1") or die(mysqli_error($sqlCon));
 }
 
 function isUserLoggedIn($name){
     global $sqlCon;
-    $query = mysqli_query($sqlCon, "SELECT `".constant("USERS_LOGGEDIN_COL")."` FROM `".constant("USERS_TABLE_NAME")."` WHERE `".constant("USERS_USERNAME_COL")."`='".$name."' AND `".constant("USERS_LOGGEDIN_COL")."`='1' LIMIT 1");
+    $query = mysqli_query($sqlCon, "SELECT `".constant("USERS_LOGGEDIN_COL")."` FROM `".constant("USERS_LOGIN_TABLE_NAME")."` WHERE `".constant("USERS_LOGIN_USERNAME_COL")."`='".$name."' AND `".constant("USERS_LOGGEDIN_COL")."`='1' LIMIT 1");
     return mysqli_num_rows($query)>0;
 }
 ?>
